4d7e309572b2cbff60f5335e9ed39be7848b46c7
Gustavo da Silva Fortunato <133658290+Delkyros@users.noreply.github.com>
2023-11-03 07:00:12 -0300

    Add files via upload

--- Node.js trainings/understanding node/index.js
+++ Node.js trainings/understanding node/index.js
@@ -27,26 +27,25 @@ function user_callback(error, user){
 }
 
 
-function get_user_phone(user_id) {
+function get_user_phone(user_id, callback) {
     setTimeout(()=>{
-        return{
-            phone: '+554835428967',
-            ddd: 48
-        }
+        return callback(null,{
+            ddd: 48,
+            phone: '+554835428967'
+        })
     },2000);
 }
 
-function get_user_address(user_id) {
+function get_user_address(user_id, callback) {
     setTimeout(()=>{
-        return{
-            number: "42",
+        return callback(null, {
             street: "A good street",
-            suite_number: 'NA',
+            number: "42",
             city: "A perfect city",
             State: 'Acre',
             postal_code: "88693145",
             country: "Brazil"
-        }
+        })
     },2000);
 }
 
@@ -54,4 +53,26 @@ function get_user_address(user_id) {
 //const user_phone = get_user_phone(user)
 //const user_address = get_user_address(user)
 
-get_user(user_callback)
+get_user(function resolve_user(error, user){
+    if(error){ //if any True error value is given
+        console.error('Something is wrong with the user data', error)
+        return;
+    }
+    get_user_phone(user.id, function resolve_user_phone(error1, phone){
+    if(error){ //if any True error value is given
+        console.error("Something is wrong with the user's phone", error1)
+        return;
+    }  
+    get_user_address(user.id, function resolve_user_address(error2, address){
+        if(error){
+        console.error("something is wrong with the user's address", error2)
+        return;
+    }
+    console.log(`
+    Name: ${user.name},
+    Address: ${JSON.stringify(address, null, 2)}
+    Phone:${JSON.stringify(phone, null, 2)}
+    `)
+    })
+    })
+})
